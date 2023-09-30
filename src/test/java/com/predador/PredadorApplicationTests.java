24d298a4b87e6b65f2266adb3b2388d8235d05a8
Érico Oliveira Chicon Silva <erico.max@gmail.com>
2023-09-30 16:28:23 -0300

    primeiro commit

---
+++ src/test/java/com/predador/PredadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
